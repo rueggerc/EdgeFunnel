fcdf666256760a4f32c6f24b57c9b6d09ae38512
Chris Ruegger <chris.ruegger@gmail.com>
2019-04-25 07:05:02 -0400

    dev

--- mqtt-kafka/KafkaBridge.py
+++ mqtt-kafka/KafkaBridge.py
@@ -31,18 +31,26 @@ def connect_kafka_producer():
         return _producer
  
 def on_mqtt_connect(client, userdata, flags, rc):
+    print("MQTT CONNECT")
     print("Connected with result code "+str(rc))
     client.subscribe(MQTT_PATH)
  
 def on_mqtt_message(client, userdata, message):
-    print("message received=", str(message.payload.decode("utf-8")))
-    payload = str(message.payload.decode("utf-8"))
-    print("The message is {}".format(payload))
+    try:
+      print("MQTT MESSAGE")
+      print("message received=", str(message.payload.decode("utf-8")))
+      payload = str(message.payload.decode("utf-8"))
+      print("The message is {}".format(payload))
     
-    # 'sensor3,64.04,99.90,1556182811311'
-    items = payload.split(",")    
-    key = items[0]
-    publish_kafka_message(kafka_producer, kafka_topic, key, payload)
+      # 'sensor3,64.04,99.90,1556182811311'
+      items = payload.split(",")    
+      key = items[0]
+      print("BEFORE PUBLISH")
+      publish_kafka_message(kafka_producer, kafka_topic, key, payload)
+    except Exception as ex:
+      print("on_mqtt_message ERROR" + str(ex))
+    finally:
+      print("on_mqtt_message DONE")
     
     
 def main():
@@ -55,8 +63,11 @@ def main():
     client.on_connect = on_mqtt_connect  
     client.on_message = on_mqtt_message
  
+    print("Client Connect")
     client.connect(MQTT_SERVER, 1883, 60)
     client.loop_forever()
+
+    print("Client Listening")
 
 if __name__ == '__main__':
     main()
